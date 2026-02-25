bb48dc45a509febf1b215fd8638d9a297dfb3e8c
Vishwas Parpattegar <vishwasparpattegar@gmail.com>
2026-02-25 13:40:12 +1100

    Initialize Home Manager flake config

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Vishwas Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations = {
+        "vishwas" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,100 @@
+{ pkgs, ... }:
+{
+  home.username = "vishwas";
+  home.homeDirectory = "/home/vishwas";
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.stateVersion = "25.05";
+
+  targets.genericLinux.enable = true;
+
+  xdg.configFile."nix/nix.conf" = {
+    force = true;
+    text = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  home.packages = with pkgs; [
+    gh
+    btop
+    bun
+    nodejs
+    fish
+    tailscale
+  ];
+
+  programs.fish = {
+    enable = true;
+    shellAbbrs = {
+      oc = "opencode";
+      hms = "home-manager switch --flake ~/nix-config#vishwas";
+      ngc = "nix-collect-garbage -d";
+      cat = "bat";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.atuin = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableFishIntegration = true;
+    icons = "auto";
+  };
+
+  programs.bat = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "Vishwas Parpattegar";
+      user.email = "vishwasparpattegar@gmail.com";
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      rebase.autoStash = true;
+    };
+  };
+
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+  };
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    profiles.default = {
+      userSettings = {
+        "terminal.integrated.profiles.linux" = {
+          "fish" = {
+            path = "${pkgs.fish}/bin/fish";
+          };
+        };
+        "terminal.integrated.defaultProfile.linux" = "fish";
+      };
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
